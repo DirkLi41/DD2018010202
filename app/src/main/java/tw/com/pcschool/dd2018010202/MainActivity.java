2660e94b84ecb65b6df84e9c12523e46c32884dc
Jhan <Jhan@gmail.com>
2018-01-03 16:04:12 +0800

    20180103延續的內容

--- app/src/main/java/tw/com/pcschool/dd2018010202/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018010202/MainActivity.java
@@ -1,15 +1,35 @@
 package tw.com.pcschool.dd2018010202;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     CheckBox cb1;
-    CheckBox cb2;
+
+    RadioGroup rg;
+
+    Switch sw1;
+    ProgressBar pb1;
+
+    ProgressBar pb2;
+
+    SeekBar sb1;
+    TextView tv1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +50,98 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        rg = (RadioGroup) findViewById(R.id.radioGroup);
+
+        sw1 = (Switch) findViewById(R.id.switch1);
+        pb1 = (ProgressBar) findViewById(R.id.progressBar);
+
+        sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                {
+                    pb1.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    pb1.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+
+        pb2 = (ProgressBar)findViewById(R.id.progressBar2);
+
+        sb1 = (SeekBar)findViewById(R.id.seekBar);
+        tv1 = (TextView)findViewById(R.id.textView);
+
+        sb1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tv1.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void click1(View v)
+    {
+        switch(rg.getCheckedRadioButtonId())
+        {
+            case R.id.radioButton:
+                Toast.makeText(MainActivity.this, "選項1已選取", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.radioButton2:
+                Toast.makeText(MainActivity.this, "選項2已選取", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.radioButton3:
+                Toast.makeText(MainActivity.this, "選項3已選取", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    public void click2(View v)
+    {
+        pb1.setVisibility(View.VISIBLE);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                try{
+                    Thread.sleep(3000);
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        pb1.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        }.start();
+
+    }
+    public void click3(View v)
+    {
+        pb2.setProgress(pb2.getProgress()-10);
+    }
+    public void click4(View v)
+    {
+        pb2.setProgress(pb2.getProgress()+10);
     }
 }
